e935e8a591fce543650d9d4e012c252a3b5845e3
patelkrishn <patelkrishn00@gmail.com>
2020-05-02 09:02:40 +0530

    home changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -141,19 +141,19 @@
         </v-card>
       </v-col>
       <v-col cols="12">
-        <v-card class="mx-auto primary" style="border-radius:8px">
+        
+        <v-card class="mx-auto primary" style="border-radius:8px" min-height="205">
           <v-card-text>
-            <span style="font-size:25px;color:white">Your earnings</span>
+            <span style="font-size:25px;color:white">Balance</span>
+            <span style="font-size:15px;color:white">(Today so far)</span>
             <v-row>
               <v-col cols="3.75">
                 <v-list-item two-line>
                   <v-list-item-content class="white--text">
-                    Today
+                    Expenses
                     <v-list-item-title
                       class="headline mb-1 white--text"
-                    >₹{{getMainChart.todaySales}}</v-list-item-title>
-                    {{(getMainChart.todaySales-getMainChart.yesterdaySales) >0 ? '+'+(getMainChart.todaySales-getMainChart.yesterdaySales) : (getMainChart.todaySales-getMainChart.yesterdaySales)}} ₹
-                    <br />vs Yesterday
+                    >₹{{getMainChart.expenseTodaySoFar}}</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </v-col>
@@ -161,12 +161,10 @@
               <v-col cols="3.75">
                 <v-list-item two-line>
                   <v-list-item-content class="white--text">
-                    This Month
+                    Earnings
                     <v-list-item-title
                       class="headline mb-1 white--text"
-                    >₹{{getMainChart.thisMonthSales}}</v-list-item-title>
-                    {{(getMainChart.thisMonthSales-getMainChart.lastMonthSales) >0 ? '+'+(getMainChart.thisMonthSales-getMainChart.lastMonthSales) : (getMainChart.thisMonthSales-getMainChart.lastMonthSales)}} ₹
-                    <br />vs Last month
+                    >₹{{getMainChart.earningTodaySoFar}}</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </v-col>
@@ -174,12 +172,10 @@
               <v-col cols="3.75">
                 <v-list-item two-line>
                   <v-list-item-content class="white--text">
-                    This Year
+                    Balance
                     <v-list-item-title
                       class="headline mb-1 white--text"
-                    >₹{{getMainChart.thisYearSales}}</v-list-item-title>
-                    {{(getMainChart.thisYearSales-getMainChart.lastYearSales) >0 ? '+'+(getMainChart.thisYearSales-getMainChart.lastYearSales) : (getMainChart.thisYearSales-getMainChart.lastYearSales)}} ₹
-                    <br />vs Last year
+                    >₹{{getMainChart.earningTodaySoFar-getMainChart.expenseTodaySoFar}}</v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </v-col>
